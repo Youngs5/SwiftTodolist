0e72ffd7a322f24941d8fe043bf2fd7e0a5c897c
Youngseok-O <oys9330@gmail.com>
2023-06-19 00:25:51 +0900

    edit 기능 추가

---
+++ SkyApp/EditContentModalView.swift
@@ -0,0 +1,20 @@
+//
+//  EditContentModalView.swift
+//  SkyApp
+//
+//  Created by 오영석 on 2023/06/18.
+//
+
+import SwiftUI
+
+struct EditContentModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditContentModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditContentModalView()
+    }
+}
